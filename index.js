b631d034442ba2e894f4a0ee637f63a7b86c70a3
ductuanxt2 <65258252+ductuanxt2@users.noreply.github.com>
2021-04-01 17:33:31 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,33 @@
+//Variables
+const fs = require("fs");
+const Discord = require("discord.js");
+
+//Bot
+const client = new Discord.Client();
+client.commands = new Discord.Collection();
+client.cooldowns = new Discord.Collection();
+
+//Command Handler
+const folder = fs.readdirSync('./commands/');
+for (const subfolders of folder) {
+	const subfolder = fs
+		.readdirSync(`./commands/${subfolders}/`)
+		.filter(file => file.endsWith('.js'));
+	for (const commands of subfolder) {
+		const command = require(`./commands/${subfolders}/${commands}`);
+		client.commands.set(command.name, command);
+	}
+}
+//Event Handler
+const eventFolder = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+for (const events of eventFolder) {
+	const event = require(`./events/${events}`);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args, client));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args, client));
+	}
+}
+
+//Login to bot, make it go online
+client.login(config.token)
