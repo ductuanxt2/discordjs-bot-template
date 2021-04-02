27ae9dab9f4bbc3ba96a93ae6f4cb0874e0559e7
ductuanxt2 <65258252+ductuanxt2@users.noreply.github.com>
2021-04-01 17:51:21 -0700

    Create ping.js

---
+++ commands/ping.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js');
+
+module.exports = {
+    name: 'ping',
+    aliases: [],
+    description: 'Show the bot speed',
+    usage: '<prefix>ping',
+    category: 'other',
+    botPermissions: "EMBED_LINKS",
+    guildOnly: true,
+    ownerOnly: false,
+    cooldown: 3,
+    execute(client, message, args) {
+        message.channel.send("🏓 Pong! `"+client.ws.ping+"`");
+    }
+}
